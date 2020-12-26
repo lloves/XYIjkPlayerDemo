c85592a70348eded3320b02df8e4021d3f97e8c6
liwsy <838569501@qq.com>
2020-12-26 11:19:01 +0800

    添加Server端播放器Demo

---
+++ Server-XYIjkPlayerDemo/app/src/main/java/com/xy/examples/xyijkplayerdemo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.xy.examples.xyijkplayerdemo;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import androidx.appcompat.app.AppCompatActivity;
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+
+public class MainActivity extends AppCompatActivity implements VideoListener{
+
+
+    IMediaPlayer mediaPlayer;
+    XYPlayerFragmentLayout xyPlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xyPlayerView = (XYPlayerFragmentLayout) findViewById(R.id.xy_player);
+        xyPlayerView.setVideoListener(this);
+
+
+        // http test address
+        // xyPlayerView.setPath("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
+
+        // rtsp test address
+        // xyPlayerView.setPath("http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4");
+
+        // rtsp phone
+        xyPlayerView.setPath("rtsp://192.168.43.1:8086");
+
+
+        try {
+            xyPlayerView.load();
+        } catch (IOException e) {
+            Toast.makeText(this,"播放失败",Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onBufferingUpdate(IMediaPlayer iMediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void onCompletion(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(IMediaPlayer iMediaPlayer) {
+        xyPlayerView.start();
+    }
+
+    @Override
+    public void onSeekComplete(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(IMediaPlayer iMediaPlayer, int i, int i1, int i2, int i3) {
+
+    }
+
+
+
+}
